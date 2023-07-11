0086c7fa0ea4b23e1a8d14572e384897d46525fb
aliasif72 <aa2061996@gmail.com>
2023-07-12 04:27:38 +0530

    added cors specifically

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const sequelize=require('./util/database');
 
 const app= express();
 
-app.use(cors());
+app.use(
+    cors({
+    origin: "http://127.0.0.1:5500", // " * " give access to all
+    methods:["GET","POST"] ,  // allow predefined methods only without it then allows all method 
+    credentials:true,     })
+);
 app.use(bodyParser.json());
 app.use('/user', userRoutes);
 sequelize
