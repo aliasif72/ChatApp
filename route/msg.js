00d28409934c9b9c57cd56cf853d934fb0c83b51
aliasif72 <aa2061996@gmail.com>
2023-07-19 16:17:23 +0530

    removed route msg error

--- route/msg.js
+++ route/msg.js
@@ -7,7 +7,7 @@ const middle = require('../middleware/auth')
 router.post('/sendMsg', middle.authenticate, msgController.sendMsg)
 router.get('/getMsg', middle.authenticate, msgController.getMsg)
 router.get('/latestMsg', middle.authenticate, msgController.latestMsg)
-router.post('/sendfile/:groupId',userAuthentication.authenticate,messageControllers.uploadFile);
+router.post('/sendfile/:groupId',middle.authenticate,msgController.uploadFile);
 
 //EXPORTS
 module.exports = router
